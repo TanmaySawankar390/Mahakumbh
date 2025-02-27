fc61050277354f570a394c4d4db3b412008178f8
Tanmay Sawankar <tanmaysawankar4441@gmail.com>
2025-02-28 04:37:30 +0530

    backend deployed

--- client/src/components/UsersList.jsx
+++ client/src/components/UsersList.jsx
@@ -10,7 +10,7 @@ const UsersList = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await fetch("http://localhost:5000/api/users");
+        const response = await fetch("https://mahakumbh-5.onrender.com/api/users");
 
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
